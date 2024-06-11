3f0df2e78d02ebf397ba3714ef8b1107fb2f43e5
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-06-11 19:00:09 -0400

    Section 5: Reverse int (#3)
    Given an integer, return an integer that is the reverse ordering of
    numbers.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// My solution #1
+// function reverseInt(n) {
+//   const isNegative = n < 0;
+
+//   const reversed = parseInt(
+//     Math.abs(n).toString().split('').reverse().join('')
+//   );
+
+//   return isNegative ? reversed * -1 : reversed;
+// }
+
+// My Solution #2
+function reverseInt(n) {
+  return (
+    parseInt(Math.abs(n).toString().split('').reverse().join('')) * Math.sign(n)
+  );
+}
 
 module.exports = reverseInt;
