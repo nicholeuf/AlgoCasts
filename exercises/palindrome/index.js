d1fabbae1b63621a4d3858cf876ab8d000d20dce
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-06-11 13:01:33 -0400

    section 4: palindromes (#2)
    Returns true if the reversed string equals the input string.

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// My solution
+function palindrome(str) {
+  return str.split('').reverse().join('') === str;
+}
+
+// Solution #2
+// Not ideal since it is not necessary to compare all characters
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - i - 1];
+//   });
+// }
 
 module.exports = palindrome;
