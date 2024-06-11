4c7b19659000af0c8e403c95bbbd74beb3311483
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-06-11 12:40:40 -0400

    S3 string reversal (#1)
    * my solution
    
    * other solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// My solution
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+// Solution #2
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// Solution #3
+// function reverse(str) {
+//   return str.split('').reduce((rev, ch) => ch + rev, '');
+// }
 
 module.exports = reverse;
