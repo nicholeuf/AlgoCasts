f724ffdce675f3f578e31f9a6786021e64afd3d8
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-06-12 19:13:22 -0400

    My solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// exclude non-alphanumeric chars, set to lower case
+const toNormalizedString = (str) => {
+  return str.replace(/[^\w]/g, '').toLowerCase();
+};
+
+// loop through each character of the string
+// keep track of count of each character in charMap
+const toCharMap = (str) => {
+  const charMap = {};
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+};
+
+function anagrams(stringA, stringB) {
+  const normStringA = toNormalizedString(stringA);
+  const normStringB = toNormalizedString(stringB);
+
+  if (normStringA.length !== normStringB.length) {
+    return false;
+  }
+
+  const charMapA = toCharMap(normStringA);
+  const charMapB = toCharMap(normStringB);
+
+  for (let char of Object.keys(charMapA)) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
